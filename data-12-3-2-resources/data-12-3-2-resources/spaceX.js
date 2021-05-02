10c53dcffa667de2610c6b3a9199657640517723
oholrohan <53494166+oholrohan@users.noreply.github.com>
2021-05-02 19:41:37 -0400

    Adding practice modules

---
+++ data-12-3-2-resources/data-12-3-2-resources/spaceX.js
@@ -0,0 +1,10 @@
+const url = "https://api.spacexdata.com/v2/launchpads";
+d3.json("samples.json").then(function(data){
+    console.log(data);
+});
+
+d3.json("samples.json").then(function(data){
+    firstPerson = data.metadata[0];
+    Object.entries(firstPerson).forEach(([key, value]) =>
+      {console.log(key + ': ' + value);});
+});
